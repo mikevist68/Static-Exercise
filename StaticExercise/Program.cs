6affacf972f378886798db9a3074fdef97c3c741
Michael Vistocci <mikevist68@yahoo.com>
2023-05-24 15:34:29 -0400

    completed

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(55.8);
+            Console.WriteLine($"Whats the conversion from celsius to {fahrenheit}");
+
+            var celius = TempConverter.FahrenheitToCelsius(20.2);
+            Console.WriteLine($"Whats the conversion from fahrenheit to {celius}");
         }
     }
 }
